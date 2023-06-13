90164db7b3317d6526d1d8b7d5a198f4a6bb54cd
Nicolas Lucio <nilucio65@gmail.com>
2023-06-13 10:12:27 -0300

    cria projeto

---
+++ src/main/java/com/nicolas/p2teste/P2testeApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.p2teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2testeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2testeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/p2teste/P2testeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.p2teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2testeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
